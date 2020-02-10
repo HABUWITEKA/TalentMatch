5e98f472fcc9610cdfaae24ade31418622d911c6
HABUWITEKA <43143832+HABUWITEKA@users.noreply.github.com>
2020-02-10 08:00:10 +0200

    new new

---
+++ index2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome To TalentMatch</title>
+	<link rel="stylesheet" type="text/css" href="css/intro.css">
+</head>
+<body>
+	<div id="box-container">
+		<a href="loginid1.php" class="links">
+<div class="box box1">
+	<img class="introimg" id="intro1" src="img/prostudent.png">
+	<p class="text">login as a student</p>
+</div>
+       </a>
+
+       <a href="loginid2.php" class="links">
+<div class="box box2">
+	<img class="introimg" id="intro1" src="img/business.png">
+	<p class="text">login as a company</p>
+</div>
+      </a>
+</div>
+<div class="copyrights">
+	<p id="copy">Copyrights <a target="_blank" href="http://talentmatch.rw/">TalentMatch.inc</a></p>
+</div>
+</body>
+</html>
